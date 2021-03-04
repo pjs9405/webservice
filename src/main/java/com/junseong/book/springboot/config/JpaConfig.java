dfd6f81725b1cf174df9c5d55e9061c52dfd1aa9
Junseong <pjs3948@gmail.com>
2021-03-04 20:57:34 +0900

    second commit

---
+++ src/main/java/com/junseong/book/springboot/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.junseong.book.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
---
+++ src/main/java/com/junseong/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.junseong.book.springboot.config.auth.dto;
+
+import com.junseong.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
